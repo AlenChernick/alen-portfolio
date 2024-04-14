25cae8bd89ac52377f7f594eebff20136a61d9c6
AlenChernick <crazyguy771@gmail.com>
2024-04-14 21:38:14 +0300

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const currentDate = new Date().toISOString();
+
+  return [
+    {
+      url: 'https://alen-portfolio.vercel.app',
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://alen-portfolio.vercel.app/about',
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://alen-portfolio.vercel.app/projects',
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.7,
+    },
+    {
+      url: 'https://alen-portfolio.vercel.app/contact',
+      lastModified: currentDate,
+      changeFrequency: 'yearly',
+      priority: 0.6,
+    },
+  ];
+}
